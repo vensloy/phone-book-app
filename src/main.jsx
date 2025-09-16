d231f5bde937528fbff4f81d1651ea9c59772a84
envoker <komogorcevanton10@gmail.com>
2025-09-16 20:53:32 +0800

    Added all the folders for the work

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
+// == Reset css
+import './reset.css'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode></StrictMode>,
 )
